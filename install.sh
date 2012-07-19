1cb15cbd8bb79ec623f369d1f75d64c05568d373
Maksym <max@max.(none)>
2012-07-19 14:50:58 +0200

    added install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get install curl awesome guake zsh git htop vim-nox postgresql postgresql-contrib pgadmin3 puppet chromium-browser openssh-server exuberant-ctags
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+chsh -s /usr/bin/zsh
+sudo apt-get install virtualbox
+sudo apt-get install recoverdm
+sudo apt-get install squashfs-tools
+sudo apt-get install xfonts-terminus
+sudo apt-get install cpufreqd
+sudo apt-get install cpufrequtils
